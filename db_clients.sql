7034ded804cad10a0b040d8927c1a81a250a0e59
Chris Lisangola <jochri3@gmail.com>
2022-07-01 15:16:23 +0300

    Ajout de la table telephones

--- db_clients.sql
+++ db_clients.sql
@@ -14,6 +14,14 @@ email VARCHAR(70) UNIQUE,
 telephone VARCHAR(16) NOT NULL 
 );
 
+-- Creation de la table telephones
+CREATE TABLE IF NOT EXISTS telephones(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    numero VARCHAR(16) NOT NULL,
+    id_client INT,
+    FOREIGN KEY(id_client) REFERENCES clients(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 -- 3 Afficher la description de la table
 -- DESC clients;
 DESCRIBE clients;
@@ -22,8 +30,25 @@ DESCRIBE clients;
 -- ALTER TABLE clients ADD COLUMN postnom VARCHAR(50) AFTER prenom;
 
 -- LMD 
-INSERT INTO clients(nom,prenom,email,telephone) VALUES("Zozor","Alain","zozor@gmail.com","0555454545"),("Lydie","Toto","toto@gmail.com","0645454545"),("Jean","Zazar","jean.zazar@hotmail.fr","0746464646");
+-- INSERT INTO clients(nom,prenom,email,telephone) VALUES("Zozor","Alain","zozor@gmail.com","0555454545"),("Lydie","Toto","toto@gmail.com","0645454545"),("Jean","Zazar","jean.zazar@hotmail.fr","0746464646");
 
-SELECT * FROM clients;
+-- Lister toutes les colonnes
+-- SELECT * FROM clients;
 
+-- Lister quelques colonnes
+-- SELECT prenom, nom FROM clients;
 
+-- Lister dans l'ordre croissant des prenom
+-- SELECT id,prenom,nom FROM clients ORDER BY prenom ASC;
+
+-- Lister les clients dont les noms commencent par "Lisa"
+-- SELECT id,prenom,nom FROM clients WHERE nom LIKE "Lisa%";
+
+-- Lister les clients dont les noms terminent par "ola"
+-- SELECT id,prenom,nom FROM clients WHERE nom LIKE "%ola";
+
+-- Lister les clients dont les email contiennent par "@gmail."
+-- SELECT id,prenom,nom,email FROM clients WHERE email LIKE "%@amazon.%";
+
+-- Fusionner les colonners prenom et nom
+-- SELECT id,CONCAT(prenom," ",nom) as nom_complet FROM clients;
